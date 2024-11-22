ada9f697f2c0dc9da1545f8098144e997c59144c
Jack davis <143725117+Weeb-trash2000@users.noreply.github.com>
2024-11-22 17:00:52 +0000

    binary searching?

---
+++ Binary Search C13/BinarySearch.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <array>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+void lenierSearch(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            cout << "element " << arr[i] << " found at index: " <<  i << '\n';
+            return;
+        }
+    }
+}
+
+void binarySearch(int arr[], int l, int r, int x){
+    if (r >= l)
+    {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+        {
+            cout << "element " << x <<" found at index: " << mid << '\n';
+            return;
+        }
+        if (arr[mid] > x)
+        {
+            binarySearch(arr, l, mid - 1, x);
+        }
+
+        else
+        {
+            binarySearch(arr, mid + 1, r, x);
+        }
+    }
+    else
+    {
+        cout << "element not found" << '\n';
+    }
+}
